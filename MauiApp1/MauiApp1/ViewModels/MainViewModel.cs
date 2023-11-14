fea1f67bbd6e0d49f4eac05c1a3bb9c0c39dc494
Chase-PocketBard <chase@pocketbard.app>
2023-11-14 11:39:11 -0500

    Possible workaround

--- MauiApp1/MauiApp1/ViewModels/MainViewModel.cs
+++ MauiApp1/MauiApp1/ViewModels/MainViewModel.cs
@@ -11,28 +11,28 @@ public partial class MainViewModel : BaseViewModel
 
     [ObservableProperty]
     public ObservableCollection<MyThing> myStuff = [
-        new MyThing { Name = "Thing 1", ImageUrl = "https://picsum.photos/200" },
-        new MyThing { Name = "Thing 2", ImageUrl = "https://picsum.photos/200" },
-        new MyThing { Name = "Thing 3", ImageUrl = "https://picsum.photos/200" },
-        new MyThing { Name = "Thing 4", ImageUrl = "https://picsum.photos/200" },
-        new MyThing { Name = "Thing 5", ImageUrl = "https://picsum.photos/200" },
-        new MyThing { Name = "Thing 6", ImageUrl = "https://picsum.photos/200" },
-        new MyThing { Name = "Thing 7", ImageUrl = "https://picsum.photos/200" },
-        new MyThing { Name = "Thing 8", ImageUrl = "https://picsum.photos/200" },
-        new MyThing { Name = "Thing 9", ImageUrl = "https://picsum.photos/200" },
-        new MyThing { Name = "Thing 10", ImageUrl = "https://picsum.photos/200" },
-        new MyThing { Name = "Thing 11", ImageUrl = "https://picsum.photos/200" },
-        new MyThing { Name = "Thing 12", ImageUrl = "https://picsum.photos/200" },
-        new MyThing { Name = "Thing 13", ImageUrl = "https://picsum.photos/200" },
-        new MyThing { Name = "Thing 14", ImageUrl = "https://picsum.photos/200" },
-        new MyThing { Name = "Thing 15", ImageUrl = "https://picsum.photos/200" },
-        new MyThing { Name = "Thing 16", ImageUrl = "https://picsum.photos/200" },
-        new MyThing { Name = "Thing 17", ImageUrl = "https://picsum.photos/200" },
+        new MyThing { Name = "Thing 1", ImageUrl = "https://picsum.photos/500" },
+        new MyThing { Name = "Thing 2", ImageUrl = "https://picsum.photos/500" },
+        new MyThing { Name = "Thing 3", ImageUrl = "https://picsum.photos/500" },
+        new MyThing { Name = "Thing 4", ImageUrl = "https://picsum.photos/500" },
+        new MyThing { Name = "Thing 5", ImageUrl = "https://picsum.photos/500" },
+        new MyThing { Name = "Thing 6", ImageUrl = "https://picsum.photos/500" },
+        new MyThing { Name = "Thing 7", ImageUrl = "https://picsum.photos/500" },
+        new MyThing { Name = "Thing 8", ImageUrl = "https://picsum.photos/500" },
+        new MyThing { Name = "Thing 9", ImageUrl = "https://picsum.photos/500" },
+        new MyThing { Name = "Thing 10", ImageUrl = "https://picsum.photos/500" },
+        new MyThing { Name = "Thing 11", ImageUrl = "https://picsum.photos/500" },
+        new MyThing { Name = "Thing 12", ImageUrl = "https://picsum.photos/500" },
+        new MyThing { Name = "Thing 13", ImageUrl = "https://picsum.photos/500" },
+        new MyThing { Name = "Thing 14", ImageUrl = "https://picsum.photos/500" },
+        new MyThing { Name = "Thing 15", ImageUrl = "https://picsum.photos/500" },
+        new MyThing { Name = "Thing 16", ImageUrl = "https://picsum.photos/500" },
+        new MyThing { Name = "Thing 17", ImageUrl = "https://picsum.photos/500" },
     ];
 
     [RelayCommand]
     public void LoadMore()
     {
-        MyStuff.Add(new MyThing { Name = $"Thing {foo++}", ImageUrl = "https://picsum.photos/200" });
+        MyStuff.Add(new MyThing { Name = $"Thing {foo++}", ImageUrl = "https://picsum.photos/500" });
     }
 }
